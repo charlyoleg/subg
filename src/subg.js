1d84a92101eac12f44a6d6f236c3816891ab710e
charles <charles.braquet@e.email>
2022-07-21 00:41:26 +0200

    remove console log

--- src/subg.js
+++ src/subg.js
@@ -5,15 +5,15 @@ import fse from 'fs-extra';
 
 async function searchGitRepo (pathDir, deepSearch = true) {
   let r_list = [];
-  console.log("gg: " + pathDir);
+  //console.log("dbg538: current pathDir: " + pathDir);
   const local_list = await fse.readdir(pathDir, {withFileTypes: true});
   for (const item of local_list) {
     if (item.isDirectory()) {
       const pathDir2 = pathDir + '/' + item.name;
       if ( ['.git', 'node_modules'].includes(item.name)) {
-	console.log("Ignore " + pathDir2);
+	//console.log("Ignore " + pathDir2);
       } else {
-	console.log(pathDir2);
+	//console.log("dbg 949: pathDir2: " + pathDir2);
 	let isRepo = false;
   	const subdirs = await fse.readdir(pathDir2, {withFileTypes: true});
   	for (const subitem of subdirs) {
@@ -22,7 +22,7 @@ async function searchGitRepo (pathDir, deepSearch = true) {
 	  }
 	}
 	if (isRepo) {
-	  console.log("Found git-repo: " + pathDir2);
+	  //console.log("Found git-repo: " + pathDir2);
 	  r_list.push(pathDir2);
 	}
 	if (deepSearch || !isRepo) {
