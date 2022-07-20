3d5c1bc81526f03f3ca16ca0c685090dcfefa551
charles <charles.braquet@e.email>
2022-07-21 00:21:52 +0200

    search for sub-repo

--- src/subg.js
+++ src/subg.js
@@ -2,25 +2,40 @@
 
 import fse from 'fs-extra';
 
-let list = [];
 
-async function searchGitRepo (pathDir) {
+async function searchGitRepo (pathDir, deepSearch = true) {
+  let r_list = [];
   console.log("gg: " + pathDir);
   const local_list = await fse.readdir(pathDir, {withFileTypes: true});
   for (const item of local_list) {
     if (item.isDirectory()) {
+      const pathDir2 = pathDir + '/' + item.name;
       if ( ['.git', 'node_modules'].includes(item.name)) {
-	console.log("Waha");
+	console.log("Ignore " + pathDir2);
       } else {
-	console.log(item.name);
-	list.push(item.name);
-	await searchGitRepo(pathDir + '/' + item.name);
+	console.log(pathDir2);
+	let isRepo = false;
+  	const subdirs = await fse.readdir(pathDir2, {withFileTypes: true});
+  	for (const subitem of subdirs) {
+    	  if (subitem.isDirectory() && (subitem.name === '.git')) {
+   	    isRepo = true;
+	  }
+	}
+	if (isRepo) {
+	  console.log("Found git-repo: " + pathDir2);
+	  r_list.push(pathDir2);
+	}
+	if (deepSearch || !isRepo) {
+	  r_list = r_list.concat(await searchGitRepo(pathDir2, deepSearch));
+	}
       }
     }
   }
+  return r_list;
 }
 
-await searchGitRepo('.');
+const list = await searchGitRepo('.');
+//const list = await searchGitRepo('.', false);
 
 console.log(list);
 
