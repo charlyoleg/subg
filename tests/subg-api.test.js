9c5ef3b3bdf073f927e991761d6b3cbc600f9a17
charles <charles.braquet@e.email>
2022-07-27 08:02:40 +0200

    fix renaming of tests

--- tests/subg-api.test.js
+++ tests/subg-api.test.js
@@ -8,12 +8,12 @@ import { Subg } from '../src/subg-api.js';
 //const repos = new Subg('.', false);
 //const repos = new Subg('.', true, 'tmp/repos.yml', '');
 //const repos = new Subg('.', true, 'tmp/repos.yml', '.');
-//const repos = new Subg('.', true, 'test/test_repos_1.yml', 'tmp3');
-const repos = new Subg('.', true, 'test/test_repos_2.yml', 'tmp3');
+//const repos = new Subg('.', true, 'tests/test_repos_1.yml', 'tmp3');
+const repos = new Subg('.', true, 'tests/test_repos_2.yml', 'tmp3');
 
 //await repos.discover_repos();
 //await repos.discover_repos('..');
-//await repos.import_yaml('test/test_repos_1.yml', 'tmp3');
+//await repos.import_yaml('tests/test_repos_1.yml', 'tmp3');
 await repos.init();
 //await repos.init('..');
 const d_list = repos.d_list();
@@ -31,7 +31,7 @@ console.log(cnd_list);
 
 //await repos.d_export_yaml('tmp5/exported_repos_1.yml');
 //await repos.d_export_yaml('tmp5/exported_repos_2.yml', true);
-//const validation_result2 = await repos.validate_yaml('test/test_repos_3.yml');
+//const validation_result2 = await repos.validate_yaml('tests/test_repos_3.yml');
 //console.log(validation_result2);
 //console.log(`subg-verison: ${Subg.version()}`);
 //process.exit(0);
@@ -53,7 +53,7 @@ await repos.d_clean();
 
 await repos.d_export_yaml('tmp5/exported_repos_1.yml');
 await repos.d_export_yaml('tmp5/exported_repos_2.yml', true);
-const validation_result = await repos.validate_yaml('test/test_repos_3.yml');
+const validation_result = await repos.validate_yaml('tests/test_repos_3.yml');
 console.log(validation_result);
 
 console.log(`subg-verison: ${Subg.version()}`);
