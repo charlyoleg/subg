452edd43edffde36270ab68228e5d49ca96b2b55
charles <charles.braquet@e.email>
2022-07-29 06:57:42 +0200

    adjust test directory tmp

--- tests/subg.test.ts
+++ tests/subg.test.ts
@@ -8,12 +8,12 @@ import { Subg } from '../src/subg-api.js';
 //const repos = new Subg('.', false);
 //const repos = new Subg('.', true, 'tmp/repos.yml', '');
 //const repos = new Subg('.', true, 'tmp/repos.yml', '.');
-//const repos = new Subg('.', true, 'tests/test_repos_1.yml', 'tmp3');
-const repos = new Subg('.', true, 'tests/test_repos_2.yml', 'tmp3');
+//const repos = new Subg('.', true, 'tests/test_repos_1.yml', 'tmp');
+const repos = new Subg('.', true, 'tests/test_repos_2.yml', 'tmp');
 
 //await repos.discover_repos();
 //await repos.discover_repos('..');
-//await repos.import_yaml('tests/test_repos_1.yml', 'tmp3');
+//await repos.import_yaml('tests/test_repos_1.yml', 'tmp');
 await repos.init();
 //await repos.init('..');
 const d_list = repos.d_list();
@@ -29,8 +29,8 @@ console.log(dnc_list);
 console.log(cnd_list);
 
 
-//await repos.d_export_yaml('tmp5/exported_repos_1.yml');
-//await repos.d_export_yaml('tmp5/exported_repos_2.yml', true);
+//await repos.d_export_yaml('tmp/exported_repos_1.yml');
+//await repos.d_export_yaml('tmp/exported_repos_2.yml', true);
 //const validation_result2 = await repos.validate_yaml('tests/test_repos_3.yml');
 //console.log(validation_result2);
 //console.log(`subg-verison: ${Subg.version()}`);
@@ -51,8 +51,8 @@ await repos.d_remote();
 await repos.d_stash_list();
 await repos.d_clean();
 
-await repos.d_export_yaml('tmp5/exported_repos_1.yml');
-await repos.d_export_yaml('tmp5/exported_repos_2.yml', true);
+await repos.d_export_yaml('tmp/exported_repos_1.yml');
+await repos.d_export_yaml('tmp/exported_repos_2.yml', true);
 const validation_result = await repos.validate_yaml('tests/test_repos_3.yml');
 console.log(validation_result);
 
