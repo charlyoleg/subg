3e15f6be985c8a18db061bf8a95b75e7c029a290
charles <charles.braquet@e.email>
2022-09-10 22:52:20 +0200

    adding eslint config

---
+++ .eslintrc.cjs
@@ -0,0 +1,8 @@
+// eslintrc.cjs
+
+module.exports = {
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  root: true,
+};
